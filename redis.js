db8c0c4d66f6542d45391b81efee1a72bbe6a2ff
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-04-22 14:29:00 -0400

    Add redis test and redis client.

---
+++ redis.js
@@ -0,0 +1,12 @@
+const winston = require('winston');
+const redisClient = require('redis').createClient(6379, 'redis');
+
+redisClient.on("error",
+  (err) => winston.error(err)
+);
+
+redisClient.on('connect',
+  () => winston.info('Connected to Redis')
+);
+
+module.exports = redisClient;
--- test/unit/redix.test.js
+++ test/unit/redix.test.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const client = require('../../redis');
 const test = require('ava');
 
-// Add tests
-test('test', t => {
+test.cb('test set redis command', t => {
   t.plan(1);
-  t.truthy(true);
+
+  client.set("name", "John Rambo");
+  client.get("name", (err, name) => {
+    t.is(name, "John Rambo");
+    t.end();
+  });
 });
