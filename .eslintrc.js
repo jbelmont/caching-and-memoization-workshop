178a2c70baf886c362bcc945606f1284f8777625
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-04-24 08:10:29 -0400

    Rewrite to use vuejs and add init files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'indent': [
+        'error',
+        2
+    ],
+    'linebreak-style': [
+        'error',
+        'unix'
+    ],
+    'quotes': [
+        'error',
+        'single'
+    ]
+  }
 };
